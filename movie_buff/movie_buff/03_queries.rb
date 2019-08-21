d32e44278c2dd17454715fc80df9cf9c5be10d61
tmockler22 <52083794+tmockler22@users.noreply.github.com>
2019-08-21 17:04:57 -0400

    Work on movie buff queries

--- movie_buff/movie_buff/03_queries.rb
+++ movie_buff/movie_buff/03_queries.rb
@@ -1,12 +1,18 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
-
+  Movie
+    .select(:id, :title)
+    .joins(:actors)
+    .where(actors: { name: those_actors })
+    .group(:id)
+    .having('COUNT(movies.id) = ?', those_actors.length)
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
 
+  
 end
 
 def costars(name)
