0ad313b492402d465132a6d974234e06180ff939
tmockler22 <52083794+tmockler22@users.noreply.github.com>
2019-08-21 19:16:40 -0400

    Seed database

--- polls_app/db/seeds.rb
+++ polls_app/db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Response.destroy_all
+AnswerChoice.destroy_all 
+Question.destroy_all 
+Poll.destroy_all 
+User.destroy_all 
+
+100.times do
+  User.create(username: Faker::Internet.unique.username)
+end
+
+user_ids = User.pluck(:id)
+300.times do 
+  Poll.create(
+    title: Faker::Book.title,
+    author_id: user_ids.sample
+  )
+end
+
+poll_ids = Poll.pluck(:id)
+900.times do 
+    Question.create(
+      poll_id: poll_ids.sample,
+      text: Faker::Lorem.question
+    )
+end
+
+question_ids = Question.pluck(:id)
+3600.times do 
+    AnswerChoice.create(
+      text: Faker::Lorem.sentence,
+      question_id: question_ids.sample
+    )
+end
+
+responder_ids = User.pluck(:id)
+answer_choice_ids = AnswerChoice.pluck(:id)
+10000.times do 
+    Response.create(
+      responder_id: responder_ids.sample,
+      answer_choice_id: answer_choice_ids.sample
+    )
+end
