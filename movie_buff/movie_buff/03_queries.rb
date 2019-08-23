53afe2ee7f2ceb17b98835a8ec404582ec22a75a
tmockler22 <52083794+tmockler22@users.noreply.github.com>
2019-08-23 18:07:07 -0400

    Complete movie_buff queries part 3

--- movie_buff/movie_buff/03_queries.rb
+++ movie_buff/movie_buff/03_queries.rb
@@ -11,8 +11,12 @@ end
 
 def golden_age
   # Find the decade with the highest average movie score.
-
-  
+  Movie
+    .group('decade')
+    .order('SUM(movies.score) / COUNT(movies.id) DESC')
+    .limit(1)
+    .pluck('(movies.yr / 10 * 10) AS decade')
+    .first
 end
 
 def costars(name)
@@ -20,11 +24,33 @@ def costars(name)
   # appeared with.
   # Hint: use a subquery
 
+  Actor
+    .joins(:castings)
+    .where([<<-SQL, name: name])
+      castings.movie_id IN (
+        SELECT
+          movies.id
+        FROM
+          movies
+        JOIN
+          castings ON castings.movie_id = movies.id
+        JOIN
+          actors ON actors.id = castings.actor_id
+        WHERE
+          actors.name = :name
+      )
+      AND actors.name != :name
+    SQL
+    .distinct
+    .pluck(:name)
 end
 
 def actor_out_of_work
   # Find the number of actors in the database who have not appeared in a movie
-
+  Actor
+    .left_outer_joins(:castings)
+    .where(castings: { movie_id: nil })
+    .count
 end
 
 def starring(whazzername)
@@ -34,6 +60,11 @@ def starring(whazzername)
 
   # ex. "Sylvester Stallone" is like "sylvester" and "lester stone" but
   # not like "stallone sylvester" or "zylvester ztallone"
+  
+  name_ex = "%#{ whazzername.split("").join("%") }%"
+  Movie
+    .joins(:actors)
+    .where("actors.name ILIKE ?", name_ex)
 
 end
 
@@ -42,5 +73,10 @@ def longest_career
   # (the greatest time between first and last movie).
   # Order by actor names. Show each actor's id, name, and the length of
   # their career.
-
+  Actor
+    .select('MAX(yr) - MIN(yr) AS career, actors.id, actors.name')
+    .joins(:movies)
+    .group(:id)
+    .order('career DESC')
+    .limit(3)
 end
